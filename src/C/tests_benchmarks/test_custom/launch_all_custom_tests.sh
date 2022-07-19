2e6458b05a16f4c8f5b6a507d58b48f72d0299c8
Laurent Lyaudet <Laurent.Lyaudet@gmail.com>
2022-07-19 16:49:58 +0200

    LL : .gitignore in benchmark_custom(_strings) and test_custom(_strings) for .c .o .exe and results, readd launch_all_custom_tests.sh scripts.

---
+++ src/C/tests_benchmarks/test_custom/launch_all_custom_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+php generateCustomTest.php < launch_all_arguments > launch_all_results
+
---
+++ src/C/tests_benchmarks/test_custom_strings/launch_all_custom_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+php generateCustomTestForStrings.php < launch_all_arguments > launch_all_results
+
